8ceccf0f4bab7e5ce34414afc4c2f56b360e2488
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-02-22 19:22:41 +0000

    (add) authors file

--- shell.h
+++ shell.h
@@ -14,12 +14,7 @@
 #define END_OF_FILE -2
 #define EXIT -3
 
-/* Global environemnt */
-extern char **environ;
-/* Global program name */
-char *name;
-/* Global history counter */
-int hist;
+
 
 /**
  * struct list_s - A new struct type defining a linked list.
@@ -93,12 +88,12 @@ int _strncmp(const char *s1, const char *s2, size_t n);
 /* Builtins */
 int (*get_builtin(char *command))(char **args, char **front);
 int shellby_exit(char **args, char **front);
-int shellby_env(char **args, char __attribute__((__unused__)) * *front);
-int shellby_setenv(char **args, char __attribute__((__unused__)) * *front);
-int shellby_unsetenv(char **args, char __attribute__((__unused__)) * *front);
-int shellby_cd(char **args, char __attribute__((__unused__)) * *front);
-int shellby_alias(char **args, char __attribute__((__unused__)) * *front);
-int shellby_help(char **args, char __attribute__((__unused__)) * *front);
+int shellby_env(char **args, char __attribute__((__unused__)) **front);
+int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
 /* Builtin Helpers */
 char **_copyenv(void);
