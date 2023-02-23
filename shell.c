dd1044959d31cb0a5e2efcf4c2bd95d6c9eb9b07
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-02-23 15:04:51 +0000

    (test) task00

--- shell.c
+++ shell.c
@@ -3,7 +3,18 @@
 #include <string.h>
 #include <unistd.h>
 
-/**
- * main - Entry point
- * 
- */
+int main(void)
+{
+    size_t size = 10;
+    char *buf = malloc(sizeof(char) * size);
+    while (1)
+    {
+        printf("$ ");
+        getline(&buf, &size, stdin);
+
+        printf("buf: %s", buf);
+    }
+    free(buf);
+
+    return 0;
+}
