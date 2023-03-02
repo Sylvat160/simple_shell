975fb60d28a3eea81f0f8414f1f980a1038efbac
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-03-02 18:04:45 +0000

    main file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+(void)argc;
+(void)argv;
+return (0);
+}
